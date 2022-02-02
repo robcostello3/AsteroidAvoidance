2b6900f6189a4ef868281c54daeb4478e4462eae
robcostello3 <80281596+robcostello3@users.noreply.github.com>
2022-02-02 12:13:59 -0500

    changed localhost

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.post('/deleteGame', function(req, res){
 })
 
 app.use(express.static(__dirname+"/views"))
-app.listen(3000, function(){
-    console.log("Listening on Port 3000")
+app.listen(5000, function(){
+    console.log("Listening on Port 5000")
 })
